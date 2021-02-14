3acc1e421ea50885c0d17f81d9446279df18d20f
max-ashton <max-ashton@users.noreply.github.com>
2021-02-14 16:52:23 +0000

    Delete products directory

--- products/src/main/java/products/repo/CategoryRepository.java
+++
@@ -1,8 +0,0 @@
-package products.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import products.domain.Category;
-
-
-public interface CategoryRepository extends JpaRepository<Category, Long> {
-}
